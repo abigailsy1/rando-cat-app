d165521fa0a89a13ee0b8abacad60c3dcadc0c23
Abby <abigailsy1@yahoo.com>
2019-05-05 20:27:15 -0700

    initial start up of app

---
+++ src/catApi.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+export function fetchRandomCat() {
+  return axios
+    .get("http://aws.random.cat/meow")
+    .then(response => response.data)
+      console.log(response)
+    .catch(error => {
+      console.log("Error fetching random cat", error);
+    })
+}
+
